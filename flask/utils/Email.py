6e7ff14e691d2f413d3602e5024ea5bc06542544
Zhou Tianxing <z.t.x@foxmail.com>
2020-10-20 15:05:43 +0800

    更新flask邮件设置

--- flask/utils/Email.py
+++ flask/utils/Email.py
@@ -11,14 +11,12 @@ from ._smtp import SMTP
 
 __mail_config = json.load(open('conf/mail.json'))
 
-__mail_server = SMTP(**__mail_config)
+__mail_server = SMTP(**__mail_config['sender'])
 
 
 def _send_email(subject: str, message: str):
     __mail_server.send(
         subject, message, 'plain',
         'Repigeons<zz.daniel@foxmail.com>',
-        {'name': 'Zhou T.x.', 'addr': 'z.t.x@foxmail.com'},
-        {'name': 'ZzDaniel', 'addr': 'zz.daniel@foxmail.com'},
-        {'name': 'Wang Fusheng', 'addr': '739286973@qq.com'},
+        *__mail_config['receivers']
     )
