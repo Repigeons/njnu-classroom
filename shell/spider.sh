352eedb2c2ed85815dd843bf73b72921d32888da
Zhou Tianxing <z.t.x@foxmail.com>
2021-01-13 01:31:17 +0800

    更正spider.sh启动参数
    Co-Authored-By: Zhou Tianxing <z.t.x@foxmail.com>

--- shell/spider.sh
+++ shell/spider.sh
@@ -5,7 +5,7 @@ export env=pro
 
 
 source env/bin/activate
-python /usr/local/src/NjnuClassroom/manage.py -run Spider
+python /usr/local/src/NjnuClassroom/manage.py --run Spider
 
 # post a request to server for latest data
 curl -X POST http://localhost:8001/reset
