bd2247ea7bef81adcde296f5f4090a7c0524fddc
Zhou Tianxing <z.t.x@foxmail.com>
2021-02-19 01:29:48 +0800

    完善校内班车时刻表功能模块

--- wechat/miniprogram/pages/explore/pages/shuttle/shuttle.ts
+++ wechat/miniprogram/pages/explore/pages/shuttle/shuttle.ts
@@ -1,124 +1,95 @@
 // explore/pages/shuttlebus/shuttlebus.js
 // 获取应用实例
 const app = getApp<IAppOption>()
-import {formatTime, getTimeSpan, getDistance} from '../../../../utils/util'
+import {getDistance} from '../../../../utils/util'
 
 Page({
   data: {
-    showMoreStation: false,
+    selectedStation: 0,
     stations_display: Array<string>(),
-    nearestStation: [{}],
-    routes: Object(),
-    stations: Object(),
-    routes1: Object(),
-    routes2: Object(),
+    stations: Array<IShuttleStation>(),
+    routes: Array<IShuttleRoute>(),
+    direction1: Array<IShuttleRoute>(),
+    direction2: Array<IShuttleRoute>(),
     direction: Boolean(),
-    hasBus: Boolean(),
-    start: String(),
-    end: String(),
-    nextShuttle: Number(),
-    intervalUpdate: Array<number>(),
-    color1_array: ['#808080', '#0090F2', '#00F324', '#F28A00'],
-    color2_array: ['#808080', '#005A8C', '#006000', '#BF6D00'],
-  },
-  
-  onLoad(/*options*/) {
+    terminus: String(),
+    scrollId: String(),
+    name2index: Object() as Record<string, number>,
+    stationDistance: Array<number>(),
+
+    nearestHint: "（离我最近）",
   },
   
   onShow() {
-    app.getShuttle().then(data => {
+    app.getShuttle().then((data: IShuttle) => {
       this.setData(data)
-      let stations_display: Array<string> = []
-      let nearestStation = [{station:"", distance:0}]
+      let name2index: Record<string, number> = {}
+      for (let stationIndex = 0; stationIndex < data.stations.length; stationIndex++) {
+        name2index[data.stations[stationIndex][0]] = stationIndex
+      }
+      this.setData({ name2index })
+
       wx.getLocation({
         type: 'gcj02',
         success: res => {
-          let list = []
-          for (let station in this.data.stations) {
-            let distance: number = getDistance({
-              latitude1: this.data.stations[station][0],
-              longitude1: this.data.stations[station][1],
+          let stations_display: Array<string> = [],
+              stationDistance: Array<number> = [],
+              nearestStation: Array<{name:string, distance:number}> = []
+          for (let stationIndex = 0; stationIndex < this.data.stations.length; stationIndex++) {
+            let element = this.data.stations[stationIndex]
+            let distance = Math.floor(getDistance({
+              latitude1: element[1],
+              longitude1: element[2],
               longitude2: res.longitude,
               latitude2: res.latitude,
-            })
-            list.push({ station, distance })
+            }))
+            nearestStation.push({ name: element[0], distance })
+            stationDistance.push(distance)
+            stations_display.push(element[0])
           }
-          list.sort((a,b)=>a.distance-b.distance)
-          list.forEach(item => {
-            stations_display.push(`${item.station}\t距离约${Math.floor(item.distance)}米`)
-          })
-          nearestStation[0] = list[0]
-          nearestStation[0]["distance"]=Math.floor(nearestStation[0]["distance"])
-          stations_display[0] += "（距离最近）"
-          this.setData({ stations_display,nearestStation })
+          nearestStation.sort((a,b)=>a.distance-b.distance)
+          for (let index = 0; index < stations_display.length; index++) {
+            if (stations_display[index] == nearestStation[0].name) {
+              stations_display[index] += this.data.nearestHint
+              this.setData({ selectedStation: index })
+              break
+            }
+          }
+          this.setData({ stations_display, stationDistance })
+          this.redirect()
         },
         fail: console.error
       })
-
-      this.direct()
     })
-    setInterval(this.intervalUpdate, 1000 * 30)
   },
 
-  direct(e?: any): void {
+  redirect(e?: boolean): void {
     if (e) {
       this.setData({ direction: !this.data.direction})
     }
-    const now = new Date()
-    const day = now.getDay()
-    const time = formatTime(now)
-    let stations = Object.keys(this.data.stations)
-    this.setData({
-      hasBus: day>0 && day<6,
-      start: !this.data.direction ? stations[0] : stations[stations.length-1],
-      end: !this.data.direction ? stations[stations.length-1] : stations[0],
-    })
-    let nextShuttle: number = -1,
-        routes: Array<Array<string>> = !this.data.direction
-        ? this.data.routes1
-        : this.data.routes2
-    if (time < routes[0][0]) {
-      nextShuttle = 0
-    } else if (time > routes[routes.length-1][0]) {
-      nextShuttle = routes.length
+    if (!this.data.direction) {
+      this.setData({
+        terminus: this.data.stations[this.data.stations.length-1][0],
+        routes: this.data.direction1
+      })
+    } else {
+      this.setData({
+        terminus: this.data.stations[0][0],
+        routes: this.data.direction2
+      })
     }
-    for (let i=1; i<routes.length;i++) {
-      if (routes[i-1][0]<=time && routes[i][0]>=time) {
-        nextShuttle = i
+    // 滚动到最近位置
+    let now = new Date()
+    for (let scrollIndex = 0; scrollIndex < this.data.routes.length; scrollIndex++) {
+      if (Date.parse(`0 ${this.data.routes[scrollIndex][0]}`) - Date.parse(`0 ${now.getHours()}:${now.getMinutes()}`) >= -15) {
+        this.setData({ scrollId: `row${scrollIndex ? scrollIndex-1 : scrollIndex}` })
+        break
       }
     }
-    this.setData({ nextShuttle, routes })
-    this.intervalUpdate()
   },
 
-  showMoreStation() {
-    this.setData({ showMoreStation: !this.data.showMoreStation })
-  },
-
-  intervalUpdate() {
-    const nextShuttle = this.data.nextShuttle
-    if (nextShuttle != -1) {
-      const now = new Date()
-      const timeSpan = getTimeSpan(now.getHours(), now.getMinutes())
-      let lastShuttleTime = (nextShuttle>0) ? this.data.routes[nextShuttle-1][0].split(':') : ':',
-          nextShuttleTime = (nextShuttle<this.data.routes.length) ? this.data.routes[nextShuttle][0].split(':') : ':'
-      this.setData({ intervalUpdate: [
-          timeSpan - getTimeSpan(+lastShuttleTime[0], +lastShuttleTime[1]),
-          getTimeSpan(+nextShuttleTime[0], +nextShuttleTime[1]) - timeSpan
-      ]})
-    }
-  },
-
-  scroll(e?: any) {
-    console.log(e)
-  },
-
-  upper(e?: any) {
-    console.log(e)
-  },
-
-  lower(e?: any) {
-    console.log(e)
+  bindStationChange(e: AnyObject): void {
+    this.setData({ selectedStation: +e.detail.value })
   },
 
   onShareAppMessage() {
