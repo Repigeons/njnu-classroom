1393623737c43e1a69df4e175405f08a295b3a83
Zhou Tianxing <z.t.x@foxmail.com>
2021-03-15 20:55:43 +0800

    md5内嵌化

--- wechat/miniprogram/utils/encrypt.ts
+++ wechat/miniprogram/utils/encrypt.ts
@@ -1,6 +1,3 @@
-import * as md5 from "md5"
+import {Md5} from "./md5"
 
-export const md5_32 = (s: string) => md5(s)
-export const md5_16 = (s: string) => md5(s).substr(8, 16)
-
-export default md5_16
+export default (str: string) => (Md5.hashStr(str) as string).substr(8, 8)
