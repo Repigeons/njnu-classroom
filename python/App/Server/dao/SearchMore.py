71276aacf6d0b3bfebb59ef2e59793a6072f5b0e
Zhou Tianxing <z.t.x@foxmail.com>
2021-03-27 00:44:57 +0800

    Update SearchMore.py
    更正参数错误

--- python/App/Server/dao/SearchMore.py
+++ python/App/Server/dao/SearchMore.py
@@ -9,6 +9,6 @@ from utils.repdbanno import select
 
 
 @select("SELECT * FROM `pro` WHERE (%s) AND (%s) AND (%s) AND (%s) AND (%s)" %
-        ('%(_day)s', '%(_jc)s', '%(_jxl)s', '%(_zylxdm)s', '%(_keyword)s)'))
+        ('%(_day)s', '%(_jc)s', '%(_jxl)s', '%(_zylxdm)s', '%(_keyword)s'))
 def search(_day: str, _jc: str, _jxl: str, _zylxdm: str, _keyword: str, **kwargs):
     _ = _day, _jc, _jxl, _zylxdm, _keyword, kwargs
