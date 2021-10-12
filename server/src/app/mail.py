aefd703993c2dff03c696374afba5322696c3529
Zhou Tianxing <zhoutianxing@aegis-data.cn>
2021-10-12 13:58:44 +0800

    Update: Windows调试不发送邮件，仅输出日志

--- server/src/app/mail.py
+++ server/src/app/mail.py
@@ -5,6 +5,7 @@
 # @Software :  PyCharm x64
 """"""
 import json
+import logging
 import socket
 
 from .app import app
@@ -18,9 +19,13 @@ __all__ = (
 
 
 async def initialize():
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-    sock.connect(smtp_socket_address)
+    try:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+        sock.connect(smtp_socket_address)
 
-    app['mail'] = lambda **kwargs: sock.send(
-        json.dumps(kwargs).encode(encoding='utf8')
-    )
+        app['mail'] = lambda **kwargs: sock.send(
+            json.dumps(kwargs).encode(encoding='utf8')
+        )
+    except AttributeError as e:
+        # Windows调试不发送邮件
+        app['mail'] = lambda **kwargs: logging.error(json.dumps(kwargs, ensure_ascii=False))
