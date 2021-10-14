975b368b0ee39fdcb92f320c5792cf68ebfed544
Zhou Tianxing <z.t.x@foxmail.com>
2021-10-14 20:33:08 +0800

    Update: orm补充类型及注释
    Signed-off-by: Zhou Tianxing <z.t.x@foxmail.com>

--- server/src/orm/Correction.py
+++ server/src/orm/Correction.py
@@ -9,15 +9,26 @@ from ._BaseModel import BaseModel
 
 class Correction(BaseModel):
     def __init__(self, row: dict):
-        self.id = row['id']
-        self.day = row['day']
-        self.JXLMC = row['JXLMC']
-        self.jsmph = row['jsmph']
-        self.JASDM = row['JASDM']
-        self.SKZWS = row['SKZWS']
-        self.zylxdm = row['zylxdm']
-        self.jc_ks = row['jc_ks']
-        self.jc_js = row['jc_js']
-        self.jyytms = row['jyytms']
-        self.kcm = row['kcm']
-        self.SFYXZX = bool(row['SFYXZX'] == b'\x01')
+        self.id: int = row['id']
+        self.day: str = row['day']
+        """Enum('Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.')"""
+        self.JXLMC: str = row['JXLMC']
+        """教学楼名称"""
+        self.jsmph: str = row['jsmph']
+        """教室门牌号"""
+        self.JASDM: str = row['JASDM']
+        """教室代码"""
+        self.SKZWS: int = row['SKZWS']
+        """上课座位数"""
+        self.zylxdm: str = row['zylxdm']
+        """资源类型代码"""
+        self.jc_ks: int = row['jc_ks']
+        """开始节次"""
+        self.jc_js: int = row['jc_js']
+        """结束节次"""
+        self.jyytms: str = row['jyytms']
+        """借用用途说明"""
+        self.kcm: str = row['kcm']
+        """课程名"""
+        self.SFYXZX: bool = bool(row['SFYXZX'] == b'\x01')
+        """是否允许自习"""
