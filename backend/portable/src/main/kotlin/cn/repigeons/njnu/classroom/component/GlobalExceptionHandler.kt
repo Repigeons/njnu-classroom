a2382f541e5c548a104c021281e706d46e2caa84
Zhou Tianxing <z.t.x@foxmail.com>
2022-04-17 17:57:01 +0800

    Update: 全局配置

--- backend/portable/src/main/kotlin/cn/repigeons/njnu/classroom/component/GlobalExceptionHandler.kt
+++ backend/portable/src/main/kotlin/cn/repigeons/njnu/classroom/component/GlobalExceptionHandler.kt
@@ -2,9 +2,10 @@ package cn.repigeons.njnu.classroom.component
 
 import cn.repigeons.njnu.classroom.common.JsonResponse
 import cn.repigeons.njnu.classroom.common.Status
-import cn.repigeons.njnu.classroom.service.MailService
 import org.slf4j.LoggerFactory
-import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.http.converter.HttpMessageConversionException
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeException
 import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -17,19 +18,17 @@ import javax.servlet.http.HttpServletRequest
  * 全局异常处理
  */
 @ControllerAdvice
-class GlobalExceptionHandler(
-    private val mailService: MailService
-) {
+class GlobalExceptionHandler {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @ResponseBody
     @ExceptionHandler(Exception::class)
     fun handle(e: Exception, request: HttpServletRequest): JsonResponse {
-        logger.error("### {} {}", request.method, request.requestURI)
+        logger.error("全局异常：{} {}", request.method, request.requestURI)
         request.parameterMap.forEach { (key: String, value: Array<String>) ->
-            logger.error("### {}={}", key, value)
+            logger.error("*****请求参数*****:{},{}", key, value)
         }
-        logger.error("### {} {}", e.message, e)
+        logger.error("*********异常信息:{},{}", e.message, e)
         return when {
             e is HttpClientErrorException -> {
                 JsonResponse(
@@ -39,37 +38,28 @@ class GlobalExceptionHandler(
             }
             e is HttpRequestMethodNotSupportedException -> {
                 JsonResponse(
-                    status = Status.METHOD_NOT_ALLOWED,
-                    message = "Method Not Allowed: ${request.method}"
+                    status = Status.BAD_REQUEST,
+                    message = "请求方法错误[${request.method}]"
                 )
             }
-            e is HttpMediaTypeNotSupportedException && request.method in listOf("POST", "PUT", "DELETE") -> {
+            request.method in listOf("POST", "PUT", "DELETE") &&
+                    (e is HttpMediaTypeException || e is HttpMessageConversionException) -> {
                 val contentType = request.getHeader("Content-Type")
                 JsonResponse(
                     status = Status.BAD_REQUEST,
-                    message = "Content-Type: $contentType"
+                    message = "请求格式错误[${contentType}]"
                 )
             }
-            e is MissingServletRequestParameterException -> {
+            e is MissingServletRequestParameterException || e is HttpMessageNotReadableException -> {
                 JsonResponse(
                     status = Status.BAD_REQUEST,
-                    message = "参数缺失: ${e.parameterName}"
+                    message = e.message
                 )
             }
             else -> {
-                val stackTrace = e.stackTrace?.joinToString("") {
-                    "\n" + "${it.className}\n" +
-                            "${it.fileName}  " +
-                            "${it.lineNumber}\n" +
-                            "${it.methodName}\n"
-                }
-                mailService.sendPlain(
-                    subject = "【南师教室】错误报告",
-                    content = e.message + e.toString() + stackTrace
-                )
                 JsonResponse(
                     status = Status.FAILED,
-                    message = "服务器异常"
+                    message = if (e.message.isNullOrEmpty()) "服务器异常" else e.message
                 )
             }
         }
