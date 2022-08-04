4b756a98ef86a34c0e55b0d601c934dd6e8bbbef
Zhou Tianxing <zhoutianxing@taizhou0523.cn>
2022-08-04 16:37:45 +0800

    Fix: ResourceUtil

--- backend/portable/src/main/kotlin/cn/repigeons/njnu/classroom/util/ResourceUtil.kt
+++ backend/portable/src/main/kotlin/cn/repigeons/njnu/classroom/util/ResourceUtil.kt
@@ -2,12 +2,14 @@ package cn.repigeons.njnu.classroom.util
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
 
 object ResourceUtil {
     fun loadResourceText(name: String): String? =
-        javaClass.getResourceAsStream(name)?.use {
-            BufferedReader(InputStreamReader(it, "UTF-8"))
-        }?.useLines { lines ->
-            lines.joinToString("")
+        javaClass.getResourceAsStream(name)?.use { stream ->
+            BufferedReader(InputStreamReader(stream, StandardCharsets.UTF_8))
+                .useLines { lines ->
+                    lines.joinToString("")
+                }
         }
 }
