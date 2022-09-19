e218ced40b63efc8e7e74c629986ed1d3eeb5a4f
Zhou Tianxing <zhoutianxing@taizhou0523.cn>
2022-09-19 21:01:50 +0800

    Fix: 研究生课没有KCM字段

--- backend/core/src/main/kotlin/cn/repigeons/njnu/classroom/model/KcbItem.kt
+++ backend/core/src/main/kotlin/cn/repigeons/njnu/classroom/model/KcbItem.kt
@@ -3,7 +3,8 @@ package cn.repigeons.njnu.classroom.model
 data class KcbItem(
     val ZYLXDM: String,
     val JC: String,
-    val KCM: String,
+    val KCM: String?,
     val ZC: String,
-    val JYYTMS: String?
+    val JYYTMS: String?,
+    val KBID: String?,
 )
