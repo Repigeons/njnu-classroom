8a5f69bc245d5d57194cae7a427bbbc9b5adf1d3
Zhou Tianxing <zhoutianxing@taizhou0523.cn>
2022-09-22 00:13:00 +0800

    Update: 使用Redis统一管理service开关

--- backend/portable/src/main/kotlin/cn/repigeons/njnu/classroom/component/ServiceSwitch.kt
+++ backend/portable/src/main/kotlin/cn/repigeons/njnu/classroom/component/ServiceSwitch.kt
@@ -1,8 +1,13 @@
 package cn.repigeons.njnu.classroom.component
 
+import cn.repigeons.njnu.classroom.service.RedisService
 import org.springframework.stereotype.Component
 
 @Component
-class ServiceSwitch {
-    var value = true
+class ServiceSwitch(
+    private val redisService: RedisService
+) {
+    var value: Boolean
+        set(value) = redisService.set("serviceSwitch", value)
+        get() = redisService.get("serviceSwitch", Boolean::class.java) == true
 }
