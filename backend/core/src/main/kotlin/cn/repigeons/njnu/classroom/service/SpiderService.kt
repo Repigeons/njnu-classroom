544739926e9384799b290ffdd4dec0946fe2c571
Zhou Tianxing <zhoutianxing@taizhou0523.cn>
2022-09-22 20:22:37 +0800

    Fix: openfeign

--- backend/core/src/main/kotlin/cn/repigeons/njnu/classroom/service/SpiderService.kt
+++ backend/core/src/main/kotlin/cn/repigeons/njnu/classroom/service/SpiderService.kt
@@ -3,10 +3,12 @@ package cn.repigeons.njnu.classroom.service
 import cn.repigeons.njnu.classroom.common.Weekday
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(
     name = "SpiderService",
     url = "http://spider:8080",
+    path = "spider",
     fallback = SpiderServiceFallback::class
 )
 interface SpiderService {
@@ -14,7 +16,12 @@ interface SpiderService {
     fun run()
 
     @GetMapping("/checkWithEhall")
-    fun checkWithEhall(jasdm: String, day: Weekday, jc: Short, zylxdm: String): Boolean
+    fun checkWithEhall(
+        @RequestParam jasdm: String,
+        @RequestParam day: Weekday,
+        @RequestParam jc: Short,
+        @RequestParam zylxdm: String
+    ): Boolean
 
     @GetMapping("/flushCache")
     fun flushCache()
