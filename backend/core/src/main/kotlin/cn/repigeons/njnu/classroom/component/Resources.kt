4e675cd3fdc88092cbf35b50f516b96243554591
Zhou Tianxing <zhoutianxing@taizhou0523.cn>
2022-09-30 00:28:24 +0800

    Fix: 初始化缓存

--- backend/core/src/main/kotlin/cn/repigeons/njnu/classroom/component/Resources.kt
+++ backend/core/src/main/kotlin/cn/repigeons/njnu/classroom/component/Resources.kt
@@ -1,7 +1,6 @@
 package cn.repigeons.njnu.classroom.component
 
 import cn.repigeons.njnu.classroom.service.CacheService
-import cn.repigeons.njnu.classroom.service.SpiderService
 import cn.repigeons.njnu.classroom.util.GsonUtil
 import cn.repigeons.njnu.classroom.util.ResourceUtil
 import org.slf4j.LoggerFactory
@@ -9,15 +8,13 @@ import org.springframework.stereotype.Component
 
 @Component
 class Resources(
-    cacheService: CacheService,
-    spiderService: SpiderService
+    cacheService: CacheService
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     val zylxdm = GsonUtil.fromJson(ResourceUtil.loadResourceText("/zylxdm.json")!!, List::class.java)
 
     init {
-        spiderService.flushCache()
         cacheService.flushClassroomList()
         cacheService.flushBuildingPosition()
         logger.info("zylxdm.json={}", zylxdm)
